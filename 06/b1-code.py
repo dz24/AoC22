f64f104f7e30cdaf2635f0f9d2f8a1e1c6f635bd
dz24 <danielzh@stud.ntnu.no>
2022-12-06 14:21:28 +0100

    06

---
+++ 06/b1-code.py
@@ -0,0 +1,25 @@
+def read_file(inp):
+    with open(inp, 'r') as read:
+        for line in read:
+            rstrip = line.rstrip()
+    return rstrip
+
+
+def solution(inp, signal_len=14):
+    line = read_file(inp)
+    signal = []
+    for idx, letter in enumerate(line):
+        signal.append(letter)
+
+        if len(signal) == signal_len:
+            if len(set(signal)) == signal_len:
+                print(idx+1, ''.join(signal))
+                break
+            else:
+                signal = signal[1:]
+
+INP0 = './a0-example.txt'
+INP1 = './a1-part1.txt'
+solution(INP1, signal_len=4)
+solution(INP1, signal_len=14)
+
