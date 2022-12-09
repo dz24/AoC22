09ab160bde81a96d496cde83adbae8b4963fc006
dz24 <danielzh@stud.ntnu.no>
2022-12-09 14:42:13 +0100

    09

--- 09/b1-code.py
+++ 09/b1-code.py
@@ -10,100 +10,46 @@ def reader(inp):
     return result
 
 
-def move_bodi(head, tail, move, idx):
-    new_head = head + move
-    new_tail = np.copy(tail)
-    euc_dist = np.sqrt(np.sum((new_head-tail)**2))
-
-    # strings0 = []
-    # idx0 = 14
-    # if idx == idx0: 
-    #     print('booger 0', new_tail, new_head)
-
-    # for i in range(6):
-    #     string = ['.']*6
-    #     if i == new_head[1]:
-    #         string[new_head[0]] = 'H'
-    #     if i == tail[1]:
-    #         string[tail[0]] = 'T'
-    #     strings0.append(''.join(string))
+def move_bodi(head, tail, move, first):
+    if first:
+        head = head + move
+    euc_dist = np.sqrt(np.sum((head-tail)**2))
 
     if euc_dist > 2:
-        diff0 = new_head-new_tail
-        diff0[abs(diff0) != 1] = 0 
-        new_tail = new_tail + diff0 #  (abs(new_head-new_tail == 1)).astype(int)
-    # if idx == idx0: 
-    #     print('booger 1', new_tail, new_head, (abs(new_head-new_tail)== 1).astype(int))
-    #     print(diff0)
-    if euc_dist > 1.5: 
-        diff1 = np.array((new_head-tail)/2, dtype=int)
-        new_tail = new_tail + diff1
+        diff0 = (head-tail)
+        diff0[abs(diff0) != 1] = 0
+        tail = tail + diff0
+    if euc_dist > 1.5:
+        tail = tail + ((head-tail)/2).astype(int)
 
-    # print(' ')
-    # print(idx)
-    # for i in reversed(range(6)):
-    #     string = ['.']*6
-    #     if i == new_head[1]:
-    #         string[new_head[0]] = 'H'
-    #     if i == new_tail[1]:
-    #         string[new_tail[0]] = 'T'
-    #     print(strings0[i], '|', ''.join(string))
-    # print(' ')
-
-    # if idx == idx0:
-    #     exit('ape')
-
-            
+    return head, tail
 
 
-    # if euc_dist > 1.5:
-    #     # print('ape a', tail, new_head, move)
-    #     diff = np.array((new_head-tail)/2, dtype=int)
-    #     if euc_dist > 2:
-    #         print(new_head, new_tail, diff) 
-    #     new_tail = new_tail + diff
-    #     if euc_dist > 2:
-    #         print('bribe a')
-    #         print(new_head, new_tail, (new_head-new_tail) == 1)
-    #         new_tail = new_tail + (new_head-new_tail) == 1
-    #         print(new_tail)
-    #         print('bribe b')
-    #         exit('bribe')
-
-        # print('ape b', new_tail, new_head)
-    # print('ape')
-    # print(f'{idx:02.0f}', ':', tail, head, '|', new_tail, new_head, move)
-    return new_head, new_tail
-
-
-def solution(inp):
+def solution(inp, length=2):
     data = reader(inp)
     history = {}
-    head, tail = np.array([0, 0]), np.array([0, 0])
+    bodi = [np.array([0, 0]) for _ in range(length)]
     nwse = {'R': [1, 0], 'L': [-1, 0],
             'U': [0, 1], 'D': [0, -1]}
     cnt = 0
     for line in data:
         move = nwse[line[0]]
-        print('-- move:', line[0], line[1], '--')
         for _ in range(line[1]):
-            head, tail = move_bodi(head, tail, move, cnt)
-            tile = f'{tail[0]}, {tail[1]}'
+            for i in reversed(range(1, length)):
+                bodi[i], bodi[i-1] = move_bodi(bodi[i], bodi[i-1],
+                                               move, i == length-1)
+            tile = f'{bodi[0][0]}, {bodi[0][1]}'
             if tile not in history:
                 history[tile] = 0
             history[tile] += 1
             cnt += 1
-        print(' ')
 
     print(sum([i>=1 for i in history.values()]))
-    # for key in history.keys():
-    #     print(key)
-    # print(history)
 
 
 
 INP0 = './a0-example.txt'
-INP1 = './a1-part1.txt'
-solution(INP1)
-
-
+INP1 = './a1-example.txt'
+INP2 = './a2-part1.txt'
+solution(INP2, length=2)
+solution(INP2, length=10)
