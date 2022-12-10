ad09ca31f4be061fc096579562cb8622cc1c7a2c
dz24 <danielzh@stud.ntnu.no>
2022-12-10 14:18:20 +0100

    10 unclean

--- 10/b2-code.py
+++ 10/b2-code.py
@@ -13,26 +13,24 @@ def solution(inp):
     tot = 1
     tot_l = []
     screen = []
-    screen_str = ''
+    screen_line = ''
     for inst in insts:
         todo = 1 if 'noop' in inst else 2
         for _ in range(todo):
-            add_str = '#' if cnt%40 in [tot+i for i in (-1, 0, 1)] else '.'
-            screen_str += add_str
+            screen_line += '#' if abs(cnt%40-tot) < 2 else '.'
             cnt += 1
+            if (cnt+20)%40 == 0:
+                tot_l.append(tot*cnt)
             if cnt%40 == 0:
-                tot_l.append(tot*cnt) 
-                screen.append(screen_str)
-                screen_str = ''
+                screen.append(screen_line)
+                screen_line = ''
         tot += 0 if todo == 1 else int(inst[1])
-    print(sum(tot_l))
-    for screen0 in screen:
-        print(screen0)
+    print('part1:', sum(tot_l))
+    print('part2:')
+    for screen_line in screen:
+        print(screen_line)
 
 
 INP0 = './a0-example.txt'
 INP1 = './a1-part1.txt'
 solution(INP1)
-
-    
-
